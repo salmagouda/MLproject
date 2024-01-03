bf6b22d0ca55ba3f91046e68c26def082f869310
salmagouda <salma.juda96@gmail.com>
2024-01-03 23:45:00 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+# settings.json
+{
+  "python.analysis.exclude": ["venv"],
+}
+
+from setuptools import find_packages,setup
+from typing import List
+
+
+hyphen_e_dot = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements=[req.replace ("\n","") for req in requirements]
+
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+
+    return requirements
+
+setup(
+name='MLproject',
+version='0.0.1',
+author = 'Salma',
+author_email= 'salma.juda96@gmail.com',
+packages= find_packages(),
+install_requires= get_requirements('requirements.txt')
+
+)
